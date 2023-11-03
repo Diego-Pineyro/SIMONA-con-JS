c3c1123772097bba5fa61e001caba6e646784e2f
vmanuelm <89673750+vmanuelm@users.noreply.github.com>
2023-11-03 16:43:27 -0300

    Delete SIMONA-con-JS directory

--- SIMONA-con-JS/contacto.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('enviarBtn').addEventListener('click', function() {
-    var confirmacion = confirm('¿Desea enviar este mensaje?');
-    
-    if (confirmacion) {
-      alert('Mensaje enviado correctamente.');
-    } else {
-      alert('Mensaje no enviado.');
-    }
-  });
